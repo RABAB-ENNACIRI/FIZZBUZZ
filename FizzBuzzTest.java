2af372713cfd32b59db53f1ba8f4971cc63771a2
RABAB-ENNACIRI <rababennaciri2002@gmail.com>
2024-12-19 04:28:48 +0100

    first commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package exercice3.TDD;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FizzBuzzTest {
+
+    // FizzBuzz.de(1)  -> "1"
+    // FizzBuzz.de(3)  -> "Fizz"
+    // FizzBuzz.de(5)  -> "Buzz"
+    // FizzBuzz.de(15) -> "FizzBuzz"
+
+    @Test
+    public void testpour1(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1",fizzBuzz.de(1) );
+    }
+
+}
